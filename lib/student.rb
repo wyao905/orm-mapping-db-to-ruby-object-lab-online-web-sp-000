9c2b6f03ac99bfa52d4d9b6b8ff44a81f848a8da
Warren Yao <warrenyca905@gmail.com>
2019-08-04 03:47:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,4 +98,15 @@ class Student
       self.new_from_db(row)
     end.first
   end
+  
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
