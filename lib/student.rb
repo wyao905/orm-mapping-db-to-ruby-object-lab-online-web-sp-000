2b1712e38dc35c19fcd70a7b2f5448cf43bb33bc
Warren Yao <warrenyca905@gmail.com>
2019-08-04 03:44:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,4 +75,16 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      LIMIT 10
+    SQL
+    
+    DB[:conn].execute(sql, 10).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
